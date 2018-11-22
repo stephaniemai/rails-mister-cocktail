5348b81d6a0db1aeedc6c797da0b6a680c7e0751
stephaniemai <stephaniemai.jacques@gmail.com>
2018-11-22 15:26:59 +0100

    new show and index actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "cranberry")
+Ingredient.create(name: "pepper")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "tonic")
+Ingredient.create(name: "vodka")
