d7eb1d037a2ccaa1599dceb4b3f7630456c28ea5
stephaniemai <stephaniemai.jacques@gmail.com>
2018-11-22 15:30:58 +0100

    cocktails#create

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -11,6 +11,16 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.new
   end
 
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.valid?
+      @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
   private
 
   def cocktail_params
